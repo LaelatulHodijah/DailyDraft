f03e5f78ec9acc3223749bccb455cfcfa25b6f2d
LaelatulHodijah <hodijah2208@gmail.com>
2023-12-08 09:08:31 +0700

    nav

--- app/src/main/java/com/project/mobile/dailydraft/SqliteHelper.java
+++ app/src/main/java/com/project/mobile/dailydraft/SqliteHelper.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SqliteHelper extends SQLiteOpenHelper{
     //DATABASE NAME
@@ -15,7 +19,11 @@ public class SqliteHelper extends SQLiteOpenHelper{
 
     //TABLE NAME
     public static final String TABLE_USERS = "users";
+    public static final String TABLE_SQLite = "sqlite";
     public static final String TABLE_DATA = "data";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_ADDRESS = "address";
 
     //TABLE USERS COLUMNS
     //ID COLUMN @primaryKey
@@ -38,6 +46,12 @@ public class SqliteHelper extends SQLiteOpenHelper{
             + KEY_EMAIL + " TEXT, "
             + KEY_PASSWORD + " TEXT"
             + " ) ";
+    public static final String SQL_TABLE_SQLITE = " CREATE TABLE " + TABLE_SQLite
+            + " ( "
+            + COLUMN_ID + " INTEGER PRIMARY KEY, "
+            + COLUMN_NAME + " TEXT, "
+            + COLUMN_ADDRESS + " TEXT"
+            + " ) ";
 
     public SqliteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -47,6 +61,7 @@ public class SqliteHelper extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         //Create Table when oncreate gets called
         sqLiteDatabase.execSQL(SQL_TABLE_USERS);
+        sqLiteDatabase.execSQL(SQL_TABLE_SQLITE);
 
     }
 
@@ -54,6 +69,53 @@ public class SqliteHelper extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         //drop table to create new one if database version updated
         sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_USERS);
+        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_SQLite);
+        onCreate(sqLiteDatabase);
+    }
+
+    public ArrayList<HashMap<String, String>> getAllData(){
+        ArrayList<HashMap<String, String>> wordList;
+        wordList = new ArrayList<HashMap<String, String>>();
+        String selectQuery = "SELECT * FROM " + TABLE_SQLite;
+        SQLiteDatabase database = this.getWritableDatabase();
+        Cursor cursor = database.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()){
+            do {
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put(COLUMN_ID, cursor.getString(0));
+                map.put(COLUMN_NAME, cursor.getString(1));
+                map.put(COLUMN_ADDRESS, cursor.getString(2));
+                wordList.add(map);
+            }while (cursor.moveToNext());
+        }
+        Log.e("select sqlite ", "" + wordList);
+        database.close();
+        return wordList;
+    }
+    public void insert (String name, String address) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        String queryValues = "INSERT INTO " + TABLE_SQLite + " (name, address) " +
+                "VALUES ('" + name + "', '" + address + "')";
+        Log.e("insert sqlite ", "" + queryValues);
+        database.execSQL(queryValues);
+        database.close();
+    }
+    public void update(int id, String name, String address){
+        SQLiteDatabase database = this.getWritableDatabase();
+        String updateQuery = "UPDATE " + TABLE_SQLite + " SET " +
+                COLUMN_NAME + "='" + name + "', " +
+                COLUMN_ADDRESS + "='" + address + "'" +
+                " WHERE " + COLUMN_ID + "=" + "'" + id + "'";
+        Log.e("update sqlite ", updateQuery);
+        database.execSQL(updateQuery);
+        database.close();
+    }
+    public void delete(int id){
+        SQLiteDatabase database = this.getWritableDatabase();
+        String updateQuery = "DELETE FROM " + TABLE_SQLite + " WHERE " + COLUMN_ID + "=" + "'" + id + "'";
+        Log.e("update sqlite ", updateQuery);
+        database.execSQL(updateQuery);
+        database.close();
     }
 
     //using this method we can add users to user table
